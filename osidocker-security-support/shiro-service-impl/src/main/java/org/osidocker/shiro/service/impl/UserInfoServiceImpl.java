4c4905bf7ffa025fdc81b25eb116f966ac46a917
caoyangjie <kingworld890719@qq.com>
2016-11-14 14:47:34 +0800

    Add some files

--- osidocker-security-support/shiro-service-impl/src/main/java/org/osidocker/shiro/service/impl/UserInfoServiceImpl.java
+++ osidocker-security-support/shiro-service-impl/src/main/java/org/osidocker/shiro/service/impl/UserInfoServiceImpl.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.osidocker.shiro.service.api.UserInfoService;
 import org.osidocker.shiro.service.entity.UserInfo;
 import org.osidocker.shiro.service.repository.UserInfoRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class UserInfoServiceImpl implements UserInfoService{
 	@Resource
 	private UserInfoRepository userInfoRepository;
 	
+	@Cacheable(value="testCache",key="'findByUsername'+#username")
 	public UserInfo findByUsername(String username) {
 		System.out.println("UserInfoServiceImpl.findByUsername()");
 		return userInfoRepository.findByUsername(username);
